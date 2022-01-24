df8e7ec070a11baef811a71f81b6f49711306928
conor <conor.lynch@ericsson.com>
2022-01-24 15:11:48 +0000

    adding the test directory to the git repo

---
+++ testconor/multiple_mix4.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 4000m -m 4096Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 5000m -m 4096Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 6000m -m 4096Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 7000m -m 4096Mi -a 1 -l 0
+
+
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 4000m -m 5120Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 5000m -m 5120Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 6000m -m 5120Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 7000m -m 5120Mi -a 1 -l 0
+
+
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 4000m -m 6144Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 5000m -m 6144Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 6000m -m 6144Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 7000m -m 6144Mi -a 1 -l 0
+
+
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 4000m -m 15360Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 5000m -m 15360Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 6000m -m 15360Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c sa -t def -d no -p 7000m -m 15360Mi -a 1 -l 0
+
+
+
+
+
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 4000m -m 4096Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 5000m -m 4096Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 6000m -m 4096Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 7000m -m 4096Mi -a 1 -l 0
+
+
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 4000m -m 5120Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 5000m -m 5120Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 6000m -m 5120Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 7000m -m 5120Mi -a 1 -l 0
+
+
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 4000m -m 6144Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 5000m -m 6144Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 6000m -m 6144Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 7000m -m 6144Mi -a 1 -l 0
+
+
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 4000m -m 15360Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 5000m -m 15360Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 6000m -m 15360Mi -a 1 -l 0
+/home/eccd/test/testAll.sh -b /home/eccd/test -s tls-off -c dist -t def -d no -p 7000m -m 15360Mi -a 1 -l 0
+
